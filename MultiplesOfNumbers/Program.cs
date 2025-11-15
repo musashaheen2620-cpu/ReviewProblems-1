24349e803e6a9835ba40b6f58d765060ad59b6a8
musashaheen2620-cpu <musashaheen2620@gmail.com>
2025-11-15 12:02:36 +0100

    Program.cs

--- MultiplesOfNumbers/Program.cs
+++ MultiplesOfNumbers/Program.cs
@@ -1,13 +1,12 @@
-﻿
-        Console.Write("Enter dividend");
-        double dividend = Convert.ToDouble(Console.ReadLine());
+﻿Console.Write("Enter dividend");
+double dividend = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Enter divisor");
-        double divisor = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter divisor");
+double divisor = Convert.ToDouble(Console.ReadLine());
 
         
-        if (dividend % divisor == 0)
-            Console.WriteLine("Multiples");
-        else
-            Console.WriteLine("Not multiples");
+if (dividend % divisor == 0)
+Console.WriteLine("Multiples");
+else
+Console.WriteLine("Not multiples");
        
